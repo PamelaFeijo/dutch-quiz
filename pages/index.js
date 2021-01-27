eac9d82c8c99a595b152e1ebb30db06d692d833d
Pamela <pamelaffeijo@gmail.com>
2021-01-27 17:08:10 +0100

    Routes and States

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Footer from '../src/components/Footer';
+import Head from 'next/head'
+import { useRouter} from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -26,15 +29,42 @@ export const QuizContainer = styled.div`
 
 
 export default function Home() {
-  return (
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
+  return ( 
   <QuizBackground backgroundImage={db.bg}> 
+    <Head>
+      <title image={db.bg}>Dutch Quiz</title>     
+    </Head>
     <QuizContainer>
+      <QuizLogo>        
+      </QuizLogo>
       <Widget>        
           <Widget.Header>
             <h1>Dutch Quiz</h1>            
           </Widget.Header> 
-            <Widget.Content>
-              <p>Find out what you know in Dutch</p>
+            <Widget.Content> 
+              <form onSubmit={function (eventInfo){
+              eventInfo.preventDefault();                     
+              router.push(`/quiz?name=${name}`);
+              console.log('Submit')
+            }}
+            >
+                <input 
+                  onChange={function (eventInfo){
+                    console.log(eventInfo.target.value)
+                    //State
+                    //name = eventInfo.target.value;
+                    setName(eventInfo.target.value)
+                  }}
+                placeholder="Tell me your name" />
+                <button type="submit" disabled={name.length === 0}>
+                  Play 
+                  {name}
+                </button>
+              </form>
           </Widget.Content>
       </Widget>
       <Widget>
